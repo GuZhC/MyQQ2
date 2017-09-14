34b1954ae1c6a2c7beab8af0e5edd82e544bdef6
ZhaiNiao <835166582@qq.com>
2017-09-14 13:02:01 +0800

    初始化项目

---
+++ Mvpstudy/app/src/main/java/com/mvpstudy/MainActivity.java
@@ -0,0 +1,72 @@
+package com.mvpstudy;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mvpstudy.book.BooksFragment;
+import com.mvpstudy.movie.MoviesFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.douban_view_pager);
+        setupViewPager(viewPager);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.douban_sliding_tabs);
+
+        if (tabLayout !=null){
+            tabLayout.addTab(tabLayout.newTab());
+            tabLayout.addTab(tabLayout.newTab());
+            tabLayout.setupWithViewPager(viewPager);
+        }
+    }
+
+    private void setupViewPager(ViewPager viewPager){
+        DoubanPagerAdapter pagerAdapter = new DoubanPagerAdapter(getSupportFragmentManager());
+        pagerAdapter.addFragment(new MoviesFragment(),getApplication().getResources().getString(R.string.tab_movies_fragment));
+        pagerAdapter.addFragment(new BooksFragment(),getApplication().getResources().getString(R.string.tab_books_fragment));
+        viewPager.setAdapter(pagerAdapter);
+    }
+
+    static class DoubanPagerAdapter extends FragmentPagerAdapter{
+
+        private final List<Fragment> mFragments = new ArrayList<>();
+        private final List<String> mFragmentTitles = new ArrayList<>();
+
+        public DoubanPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        public void addFragment(Fragment fragment,String title){
+            mFragments.add(fragment);
+            mFragmentTitles.add(title);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragments.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitles.get(position);
+        }
+    }
+}
